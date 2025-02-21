ba541646a026f095ca9656ba97399d3be774224f
Walter Mikula <146771447+walterMikula@users.noreply.github.com>
2025-02-21 13:12:34 -0600

    Update TreeProblems.java

--- TreeProblems.java
+++ TreeProblems.java
@@ -1,5 +1,5 @@
 /*
- * *** YOUR NAME GOES HERE / YOUR SECTION NUMBER ***
+ * *** Walter Mikula / 003 ***
  *
  * This java file contains several simple tree problems that need to be
  * codified. These routines  must use the TreeMap and TreeSet library
